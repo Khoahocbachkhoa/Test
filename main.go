8c05867c3b9c8b3af0441bc8bbd67972f5f29f60
Khoahocbachkhoa <Khoa.VD235354@sis.hust.edu.vn>
2026-08-31 17:03:03 +0700

    Register route

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 	"os"
+	"project/internal/handler"
 	"project/internal/repository"
 	"project/internal/service"
 
@@ -30,4 +32,21 @@ func main() {
 	taskService := service.NewTaskService(taskRepo)
 
 	// create handler
+	taskHandler := handler.NewTaskHandler(taskService)
+
+	// register route
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", taskHandler.Health)
+	mux.HandleFunc("GET /tasks", taskHandler.GetTasks)
+	mux.HandleFunc("POST /tasks", taskHandler.CreateTask)
+	mux.HandleFunc("GET /tasks/{id}", taskHandler.GetTaskByID)
+	mux.HandleFunc("PUT /tasks/{id}", taskHandler.UpdateTask)
+	mux.HandleFunc("DELETE /tasks/{id}", taskHandler.DeleteTask)
+
+	log.Println("Server listening on :8080")
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 }
